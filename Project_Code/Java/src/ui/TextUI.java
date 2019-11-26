a968cf9475a4a53acdd49e189f2cdadc712f140f
Mark Burrell <markrb0609@gmail.com>
2019-11-25 20:59:33 -0700

    Begin development of the text command line for adding appointment blocks

--- Project_Code/Java/src/ui/TextUI.java
+++ Project_Code/Java/src/ui/TextUI.java
@@ -1,5 +1,32 @@
 package ui;
 
-public class TextUI {
+import java.time.LocalDateTime;
+import java.util.Scanner;
+import database.DatabaseConnector;
 
+
+public class TextUI {
+	Scanner input;
+	int ID;
+	DatabaseConnector DB;
+	private void addNewBlock(LocalDateTime start, LocalDateTime end) {
+		String query = String.format("INSERT INTO appointments(startTime, endTime, offerdBy) VALUES (&s, %s, %d);", start.toString(), end.toString(), ID);
+		DB.runQuery(query);
+	}
+	private void displayTakenAppointments() {
+		
+	}
+	private void removeBlock(LocalDateTime start, LocalDateTime end) {
+		
+	}
+	public TextUI(Scanner in, int ID, DatabaseConnector DB) {
+		input = in;
+		this.ID = ID;
+		this.DB = DB;
+	}
+	public void driver() {
+	
+	}
+	
+	
 }
