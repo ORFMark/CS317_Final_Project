1d871aef5cd49d66f537ba1af2749d41526fdb13
Mark Burrell <markrb0609@gmail.com>
2019-11-26 13:25:13 -0700

    start designing the test population

---
+++ Project_Code/Scaffolding/databaseSamplePopulate.sql
@@ -0,0 +1,16 @@
+INSERT INTO people(firstName, lastName, isTutor, bio) VALUES 
+("Mark", "Burrell", TRUE, "Junior SE student"),  /* 1 */
+("Kaylie", "Prieur", TRUE, "Senior Buisness Student"), /* 2 */
+("Luke", "Baird", TRUE, "Junior EE Student"); /* 3 */
+
+INSERT INTO people(firstName, lastName) VALUES 
+("Joe", "Burrell"), /* 4 */
+("Paul", "Silva"), /* 5 */
+("Matt", "Jaffe"); /* 6 */
+
+INSERT INTO courses(courseCode, courseName) VALUES
+("CEC320", "Microprocessor System"),
+("EC211", "Microeconmics"),
+("CS125", "C programming");
+
+
