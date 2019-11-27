c6981049449503c3062a00c6e274a5c00d8f8deb
Mark Burrell <markrb0609@gmail.com>
2019-11-27 16:37:15 -0700

    finished developing the test data for the database

--- Project_Code/Scaffolding/databaseSamplePopulate.sql
+++ Project_Code/Scaffolding/databaseSamplePopulate.sql
@@ -11,6 +11,24 @@ INSERT INTO people(firstName, lastName) VALUES
 INSERT INTO courses(courseCode, courseName) VALUES
 ("CEC320", "Microprocessor System"),
 ("EC211", "Microeconmics"),
-("CS125", "C programming");
+("CS125", "C programming"),
+("BA332", "Corprate Finance");
 
+INSERT INTO review (tutorID, rating, review) VALUES 
+(1, 4, "MRB test 4str Reveiw"),
+(2, 5, "KRP test 5str Review"),
+(1, 5, "MRP test 5str Review");
 
+INSERT INTO tutored (tutorID, courseCode) VALUES 
+(1, "CEC320"),
+(1, "CS125"),
+(2, "CEC320"),
+(3, "EC211"),
+(3, "BA332");
+
+INSERT INTO appointments (startTIme, endTime, offeredBy, takenBy, course) VALUES
+  ('2019-12-01 09:00:00', '2019-12-01 13:00:00', 1, NULL, NULL),
+  ('2019-12-01 13:00:00', '2019-12-01 14:00:00', 1, 4, "CS125"),
+  ('2019-12-01 14:00:00', '2019-12-01 18:00:00', 1, NULL, NULL),
+  ('2019-12-01 09:00:00', '2019-12-01 18:00:00', 3, NULL, NULL),
+  ('2019-12-01 10:00:00', '2019-12-01 11:00:00', 2, 5, "BA332");
