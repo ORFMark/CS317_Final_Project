bf34a16d4f041a6a456c2c86ee1eea2d9a43ca19
Mark Burrell <markrb0609@gmail.com>
2019-11-27 22:03:14 -0700

    more work on textUI

--- Project_Code/Java/src/Main.java
+++ Project_Code/Java/src/Main.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
+
+import ui.TextUI;
+
 import database.DatabaseConnector;
 
 public class Main {
 	public static void main(String[] args) {
 		try {
 			DatabaseConnector tutorDB = new DatabaseConnector();
+			tutorDB.close();
+			Scanner scan = new Scanner(System.in);
+			TextUI text = new TextUI(scan, tutorDB);
+			text.driver();
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("THINGS WENT WRONG");
 		}
+	
+		
 	}
 }
--- Project_Code/Java/src/ui/TextUI.java
+++ Project_Code/Java/src/ui/TextUI.java
@@ -1,37 +1,74 @@
 package ui;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.Scanner;
 import database.DatabaseConnector;
-
 
 public class TextUI {
 	Scanner input;
 	int ID;
 	DatabaseConnector DB;
-	private void addNewBlock(LocalDateTime start, LocalDateTime end) {
-		String query = String.format("INSERT INTO appointments(startTime, endTime, offerdBy) VALUES (&s, %s, %d);", start.toString(), end.toString(), ID);
-		DB.runQuery(query);
-	}
-	private void displayAvalibleAppointments(String courseCode) {
-		String query = String.format("SELECT startTime, endTime from appointments where offeredBy = %D", ID);
-		DB.runQuery(query);
-	}
-	private void displayTakenAppointments() {
-		
-	}
-	private void removeBlock(LocalDateTime start, LocalDateTime end) {
-		
-	}
+	boolean isTutor;
+
 	public TextUI(Scanner in, DatabaseConnector DB) {
 		input = in;
 		this.DB = DB;
 	}
-	public void driver() {
-	    String userIn;
-	    boolean isTutor = false;
-	    System.out.print("Please enter your System ID");
+	
+	private boolean auth() {
+		String query = String.format("SELECT * from people WHERE ID = %d;", ID);
+		ResultSet result = DB.runQuery(query);
+		try {
+			boolean empty = !result.next();
+			if (result != null && !empty) {
+				isTutor = result.getBoolean("isTutor");
+				return true;
+			} else {
+				System.out.println("RS: " + Boolean.toString(result != null) + " EMPTY: " + empty);
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
-	
-	
+
+	private void addNewBlock(LocalDateTime start, LocalDateTime end) {
+		String query = String.format("INSERT INTO appointments(startTime, endTime, offerdBy) VALUES (&s, %s, %d);",
+				start.toString(), end.toString(), ID);
+		DB.runQuery(query);
+	}
+
+	private void displayAvalibleAppointments(String courseCode) {
+		String query = String.format("SELECT startTime, endTime from appointments where offeredBy = %D", ID);
+		DB.runQuery(query);
+	}
+
+	private void displayTakenAppointments() {
+
+	}
+
+	private void removeBlock(LocalDateTime start, LocalDateTime end) {
+
+	}
+
+
+	public void driver() {
+		String userIn;
+		System.out.print("Please enter your System ID");
+		ID = input.nextInt();
+		if (!auth()) {
+			System.out.println("Im sorry, that ID is not in the database");
+			return;
+		}
+		if (isTutor) {
+
+		} else {
+
+		}
+
+	}
+
 }
